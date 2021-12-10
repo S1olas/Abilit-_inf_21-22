b1707307265293cc8c90138ee8a1069008add8b8
Siolas <95907501+S1olas@users.noreply.github.com>
2021-12-10 10:35:21 +0000

    main.py

---
+++ src/main.py
@@ -0,0 +1,46 @@
+a = 3
+b = 5
+a+b
+print(a+b)
+
+d = 3+5j
+d+a
+print(d+a)
+print(type(d))
+
+
+t = True
+k = True
+
+if(t and k):
+  print(d)
+
+
+  array=[1,3,8]
+  if(9 in array): 
+    print("ok")
+  else:
+    print("no")
+    
+    
+import math
+print(math.pi)
+
+print(d**2)
+
+math.sin(1)
+print(math.sin(math.pi/2))
+r = float(input("Raggio="))
+
+def areacerchio(r):
+
+ area =(r**2)*math.pi
+ print(area)
+
+areacerchio(r)
+
+
+s = "hello"
+s1 = "world"
+
+print(s+s1)
